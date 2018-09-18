5cf6b6bbc6d4cb72418c286369c10f99e4620b5a
Christin Zasada <christin.zasada@mdc-berlin.de>
2018-09-18 11:19:54 +0200

    updated config_mtx settings in MTXQCreports

--- R/MTXQC_config.R
+++ R/MTXQC_config.R
@@ -1,10 +1,10 @@
 ### Import of config_files
 
 #conversion list    
-con_se = read.csv("config_files/conversion_metabolite.csv", TRUE)
+con_se = read.csv("config_mtx/conversion_metabolite.csv", TRUE)
 
 if (ncol(con_se) == 1) {
-  con_se = read.csv("config_files/conversion_metabolite.csv", T, sep = ";")
+  con_se = read.csv("config_mtx/conversion_metabolite.csv", T, sep = ";")
   message("Colon separator detected in : conversion_metabolite.csv")
 }
 
@@ -12,14 +12,12 @@ file_spec = read.csv("config_files/conv_filenames.csv", TRUE)
 file_spec_manVal = read.csv("config_files/conv_filenames_manVal.csv", TRUE)
 
 #Metabolic profile: relation of intermediates and pathways
-pathway_profile  =  read.csv('config_files/letter_pathway_complete.csv', T)
+pathway_profile  =  read.csv('config_mtx/letter_pathway_complete.csv', T)
 pathway_profile = pathway_profile[, 1:7]
 
-#correction Table for Quant1:1
-#qt = read.csv('config_files/quant1_values.csv', T)
 
 #reference file summarising mass of interest
-mass_li = read.csv('config_files/incorp_calc_masses.csv',  T)
-backup_mids = read.csv('config_files/mid_backups.csv', T)
+mass_li = read.csv('config_mtx/incorp_calc_masses.csv',  T)
+backup_mids = read.csv('config_mtx/mid_backups.csv', T)
 
 
