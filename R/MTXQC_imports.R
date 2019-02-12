e05e5445239e98ec127b39549dd850806ec72296
Christin Zasada <christin.zasada@mdc-berlin.de>
2019-02-12 12:38:48 +0100

    Further refinement import function

--- R/MTXQC_imports.R
+++ R/MTXQC_imports.R
@@ -10,3 +10,16 @@ for (file in files) {
     gsub(" ","",substr(file, 1, perpos - 1)), 
     read.csv(paste(path,file,sep = "")))
 }
+
+
+### import params
+files <- list.files(path = path_setup, pattern = "*.csv")
+
+for (file in files) {
+  extr <- which(strsplit(file, "")[[1]] == ".")
+  assign(
+    gsub(" ","",substr(file, 1, extr - 1)), 
+    read.csv(paste(path_setup, file, sep = "")))
+  message("Imported: ", file)
+}
+
